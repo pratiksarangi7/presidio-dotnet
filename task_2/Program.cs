b061886d46a7d3dc62b33e314a3ee45ec4a0f104
Pratik <sarangipratikcode@gmail.com>
2026-04-09 12:31:30 +0530

    task 2 done

---
+++ task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace task_2;
+
+
+public class Person
+{
+    private readonly string name;
+    private readonly int age;
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void Greeting()
+    {
+        Console.WriteLine($"Hello! I am {name}. I am {age} years old");
+    }
+    static void Main(string[] args)
+    {
+        Person p1 = new("Pratik", 22);
+        Person p2 = new("Sagnik", 30);
+        Person p3 = new("Bhavya", 40);
+        Person p4 = new("Mayur", 102);
+        p1.Greeting();
+        p2.Greeting();
+        p3.Greeting();
+        p4.Greeting();
+    }
+}
+

c391834c2f2008e848576a9ac21e508f909181b2
Pratik <sarangipratikcode@gmail.com>
2026-04-10 17:17:37 +0530

    task 7 done

---
+++ task_7/Program.cs
@@ -0,0 +1,27 @@
+﻿static async Task<string> WaitForWork(int time, string workName)
+{
+    await Task.Delay(time);
+    return $"Finished {workName}.";
+}
+
+try
+{
+    var task1 = WaitForWork(2000, "Database");
+    var task2 = WaitForWork(2000, "API");
+    var task3 = WaitForWork(2000, "Cloud");
+
+    var results = await Task.WhenAll(task1, task2, task3);
+    foreach (var item in results)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (OperationCanceledException)
+{
+
+    Console.WriteLine("Operation was cancelled");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"An error Occurred!{e.Message}");
+}

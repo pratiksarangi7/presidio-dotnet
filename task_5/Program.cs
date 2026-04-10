07ec525f6cfc6898fbe50149bcf53e0e15198938
Pratik <sarangipratikcode@gmail.com>
2026-04-10 11:37:10 +0530

    task_5 done

---
+++ task_5/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    var lines = File.ReadLines("xyz.csv").Skip(1).ToList();
+    int numLines = lines.Count;
+    int numWords = 0;
+    foreach (var line in lines)
+    {
+        var words = line.Split(',');
+        numWords += words.Length;
+    }
+    string output = $"Number of words: {numWords}, Number of lines: {numLines}";
+    File.WriteAllText("data.txt", output);
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"File Not found: {ex.Message}");
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"IO Exception: {ex.Message} ");
+}
